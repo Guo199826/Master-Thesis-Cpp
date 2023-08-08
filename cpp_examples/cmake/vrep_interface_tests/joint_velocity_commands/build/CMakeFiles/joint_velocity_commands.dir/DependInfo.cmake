b9b9b161882655fc020dd995a6a65d904cebcba9
Guo199826 <yuguo199826@163.com>
2023-08-08 10:50:38 +0200

    Add test_thread_controller.cpp & test compile with opt

--- cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/DependInfo.cmake
+++ cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/DependInfo.cmake
@@ -1,29 +1,30 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/FrankaRobot.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/FrankaRobot.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/geomJac.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/geomJac.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/jacobianEst.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/jacobianEst.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/jacobianEstVector.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/jacobianEstVector.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/logmap.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/logmap.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/manipulabilityJacobian.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/manipulabilityJacobian.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/redManipulabilityJacobian.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/redManipulabilityJacobian.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/spdToVec.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/spdToVec.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/test_thread_controller.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/test_thread_controller.cpp.o"
+  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/tmprod.cpp" "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/build/CMakeFiles/joint_velocity_commands.dir/src/tmprod.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include/osqp"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/FrankaRobot.cpp" "CMakeFiles/joint_velocity_commands.dir/src/FrankaRobot.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/FrankaRobot.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/dq2tfm.cpp" "CMakeFiles/joint_velocity_commands.dir/src/dq2tfm.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/dq2tfm.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/geomJac.cpp" "CMakeFiles/joint_velocity_commands.dir/src/geomJac.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/geomJac.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/jacobianEst.cpp" "CMakeFiles/joint_velocity_commands.dir/src/jacobianEst.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/jacobianEst.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/jacobianEstVector.cpp" "CMakeFiles/joint_velocity_commands.dir/src/jacobianEstVector.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/jacobianEstVector.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/logmap.cpp" "CMakeFiles/joint_velocity_commands.dir/src/logmap.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/logmap.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/manipulabilityJacobian.cpp" "CMakeFiles/joint_velocity_commands.dir/src/manipulabilityJacobian.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/manipulabilityJacobian.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/manipulability_qp_controller.cpp" "CMakeFiles/joint_velocity_commands.dir/src/manipulability_qp_controller.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/manipulability_qp_controller.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/readData.cpp" "CMakeFiles/joint_velocity_commands.dir/src/readData.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/readData.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/redManipulabilityJacobian.cpp" "CMakeFiles/joint_velocity_commands.dir/src/redManipulabilityJacobian.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/redManipulabilityJacobian.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/spdToVec.cpp" "CMakeFiles/joint_velocity_commands.dir/src/spdToVec.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/spdToVec.cpp.o.d"
-  "/home/guo/Master-Thesis-Cpp/cpp_examples/cmake/vrep_interface_tests/joint_velocity_commands/src/tmprod.cpp" "CMakeFiles/joint_velocity_commands.dir/src/tmprod.cpp.o" "gcc" "CMakeFiles/joint_velocity_commands.dir/src/tmprod.cpp.o.d"
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
